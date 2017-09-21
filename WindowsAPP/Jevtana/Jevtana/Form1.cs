048b69aa69f45e2a3ddce9bd0b6d965ca27aa942
Kerem Yavuz <=>
2017-09-21 14:31:07 +0300

    kurulum

---
+++ WindowsAPP/Jevtana/Jevtana/Form1.cs
@@ -0,0 +1,57 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Jevtana
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
+            InitializeComponent();
+            this.ResumeLayout(false);
+        }
+
+        ChromiumWebBrowser chrome;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /* string curDir = Directory.GetCurrentDirectory();
+             Uri lnk = new Uri(String.Format("file:///{0}/www/index.html", curDir));
+             webBrowser1.Navigate(lnk);
+             */
+            string curDir = Directory.GetCurrentDirectory();
+
+            CefSettings settings = new CefSettings();
+
+
+            Cef.AddCrossOriginWhitelistEntry("file:///{0}/www/index.html", "http", "localhost:49203", true);
+            Cef.Initialize(settings);
+            chrome = new ChromiumWebBrowser(String.Format("file:///{0}/www/index.html", curDir));
+            chrome.BrowserSettings.FileAccessFromFileUrls = CefState.Enabled;
+            chrome.BrowserSettings.UniversalAccessFromFileUrls = CefState.Enabled;
+            chrome.BrowserSettings.WebSecurity = CefState.Disabled;
+            chrome.BrowserSettings.Javascript = CefState.Enabled;
+            this.panel1.Controls.Add(chrome);
+            chrome.Dock = DockStyle.Fill;
+
+        }
+        
+        
+    }
+}
